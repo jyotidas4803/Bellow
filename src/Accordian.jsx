6f3db17e85b0c755e5785ab9c6f04cff21ee2cbc
Jyotiii Das <163004395+jyotidas4803@users.noreply.github.com>
2024-11-11 07:16:21 +0000

    detail and summery

--- src/Accordian.jsx
+++ src/Accordian.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Accordian() {
   return (
-    <div>Accordian</div>
+    <div className='bg-pink-500 p-4 rounded-lg'>
+        <h1>Accordian</h1>
+        <details>
+            <summary>Click me</summary>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, quia, voluptas, quisquam, blanditiis, non, dolorum, sit, amet, consectetur, adipisicing, elit, sed, do, eiusmod, tempor, incididunt, ut, labore, et, dolore, magna, aliqua. Ut, enim, ad, minima, veniam, quis, nostrud, exercitation, ullamco, laboris, nisi, ut, aliquip, ex, ea, commodo, consequat, duis, aute, velit, irure, dolor, in, reprehenderit, in, voluptate, velit, esse, cillum, dolore, eu, fugiat, nulla, pariatur, excepteur, sint, occaecat, cupidatat, non, proident, sunt, in, culpa, qui, officia, deserunt, mollit, anim, id, est, laborum.</p>
+        </details>
+         
+    </div>
+    
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Accordian from './Accordian';
 
 function App() {
   return (
     <div>
       <h1>Hello Bellow!</h1>
+      <Accordian />
     </div>
   );
 }
